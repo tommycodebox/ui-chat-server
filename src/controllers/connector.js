97c94f4cf0d1733ac87e0530759e53174bd2ea97
Tommy <tommycodebox@gmail.com>
2019-12-19 00:33:56 +0100

    Now users can exchange messages

--- src/controllers/connector.js
+++ src/controllers/connector.js
@@ -4,6 +4,10 @@ const joinChat = require('./joinChat');
 const connector = (socket, io) => {
   socket.on('join-chat', user => joinChat(user, socket, io));
 
+  socket.on('new-message', msg => {
+    socket.broadcast.emit('message', msg);
+  });
+
   socket.on('diconnect', disconnector);
 };
 
