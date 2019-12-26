10f58bdd807ae8b1da6983853c424debea936236
Tommy <tommycodebox@gmail.com>
2019-12-26 11:50:08 +0100

    Changed exit codes

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ io.on('connection', socket => connector(socket, io, DEFAULT_TIMEOUT));
 process.on('SIGINT', () => {
   console.log('---Closing active connections---');
   io.close();
-  process.exit(1);
+  process.exit(0);
 });
 process.on('SIGTERM', () => {
   console.log('---Closing active connections---');
   io.close();
-  process.exit(1);
+  process.exit(0);
 });
